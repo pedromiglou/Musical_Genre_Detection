697eef275953dd631f2f5ad72778a3aab827709a
Pedro Amaral <pedromiglou@hotmail.com>
2022-07-04 16:35:13 +0100

    hyper-parameter optimization

--- utils.py
+++ utils.py
@@ -1,4 +1,14 @@
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+
+def split(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25)
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
 
 def plot_accuracy_comparison(accs, title, legend):
     epochs = len(accs[0])
